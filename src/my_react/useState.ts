af85a675bf159e18cc20778ba40ccbc4e0265e04
geniusgo <gogenius28@naver.com>
2024-12-21 15:32:44 +0900

    주석 추가

--- src/my_react/useState.ts
+++ src/my_react/useState.ts
@@ -14,20 +14,24 @@ const rerender = () => {
 };
 
 const useState = <T>(init: T) => {
+  // App 재호출 될 때 useState 다시 호출되더라도 관리되던 state 유지해주기
   if (isFirst) {
     state = init;
     isFirst = false;
   }
 
+  // 외부에서 state에 직접 접근하지 못하도록 getter 함수로 처리, 즉시 실행 함수로 만들어서 값을 바로 반환
   const getState = (() => {
     return state as T;
   })();
 
+  // 새로운 값이 들어오면 클로저로 관리되는 state 업데이트하고 리렌더링
   const setState = (newState: T) => {
     state = newState;
     rerender();
   };
 
+  // 즉시 실행 함수 getState의 값과 setState 함수 외부로 반환
   return [getState, setState] as [T, (newState: T) => void];
 };
 
